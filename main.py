7d0b6d95ff876494bea27c0fd3819176b134dc57
SSORBOB <57416682+SSORBOB@users.noreply.github.com>
2024-01-03 02:39:53 +0800

    -

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 def run():
     intents = discord.Intents.default()
     intents.message_content = True
+    intents.members = True
 
     bot = commands.Bot(command_prefix="/", intents=intents)
 
@@ -22,17 +23,32 @@ def run():
         brief="Collating available days for raid attendance",
         description="Bot sends a message into a location where command is called and tags @everyone, asking to react for available days to be scheduled for raids.\nThis is generally recommended to be run in threads to not clog chats and ping people in excess since it uses @everyone.",
     )
-    async def schedule(ctx):
+    async def schedule(ctx, arg="@everyone"):
         print(
             f"Command /schedule has been called by {ctx.message.author}({ctx.message.author.id}) in {ctx.guild.name}({ctx.guild.id}), channel {ctx.channel}({ctx.channel.id}) at {datetime.now()}"
         )
+
+        # Check if the argument provided is a valid role, otherwise tag everyone
+        roles = await get_roles(ctx)
+        if arg.startswith("<@&") and arg.endswith(">"):
+            arg_id = arg[3:-1]
+            # print(arg_id)
+            for role in roles:
+                if str(role.id) == arg_id:
+                    break
+        else:
+            arg = "@everyone"
+
         await ctx.message.delete()
+        # Check if arg is a valid prefix for a role, otherwise make sure it is set to everyone. It has to be a valid role.
 
         emojis = settings.emojis
-        msg = await ctx.send("@everyone, please react with your available days!")
+        msg = await ctx.send(f"{arg}, please react with your available days!")
         for emoji in emojis:
             await msg.add_reaction(emoji)
+        await msg.pin(reason=None)
 
+    # /gen
     @bot.command(
         brief="Display overall schedule of all members who reacted",
         description="Bot sends an embed with all the members who reacted to the days showing when they're available and not.\nThis command also only generates the latest called schedule in the channel.",
@@ -41,21 +57,57 @@ def run():
         print(
             f"Command /gen has been called by {ctx.message.author}({ctx.message.author.id}) in {ctx.guild.name}({ctx.guild.id}), channel {ctx.channel}({ctx.channel.id}) at {datetime.now()}"
         )
+
+        # Delete command initialization message
         await ctx.message.delete()
+
+        # Get all guild roles
+        roles = await get_roles(ctx)
+        roles_dict = {}
+        rev_roles_dict = {}
+        for role in roles:
+            roles_dict[str(role.id)] = str(role.name)
+            rev_roles_dict[str(role.name)] = str(role.id)
 
         flag = False  # Flag to keep track of whether the message has been found.
 
         # Set oldest_first = False so that we only keep track of the latest schedule collated
         async for message in ctx.channel.history(oldest_first=False):
             # print(message)
+            # Delete command initialization message
             if message.author.bot:
                 # print(message.content)
-                if message.content.startswith(
-                    "@everyone, please react with your available days!"
-                ):
+                if message.content.endswith("please react with your available days!"):
+                    mentioned_role = str(message.content).split()[0][:-1]
+                    if mentioned_role == "@everyone":
+                        mentioned_role = rev_roles_dict[mentioned_role]
+                    else:
+                        mentioned_role = mentioned_role[3:-1]
+                    print(f"{roles_dict[mentioned_role]} has been mentioned!")
+
+                    role_members = {}
+                    if mentioned_role == "everyone":
+                        # use channel members
+                        for member in ctx.channel.members:
+                            role_members[str(member.name)] = str(member.id)
+                    else:
+                        for role in roles:
+                            if str(role.id) == mentioned_role:
+                                for member in role.members:
+                                    role_members[str(member.name)] = str(member.id)
+                    # print(role_members)
+
                     flag = True  # Latest schedule message has been found
 
                     user_set, react_dict = await collate_table(message)
+
+                    # now do a diff check for user_set and role_members
+                    remind_members = []
+                    for member in role_members.keys():
+                        if member not in user_set:
+                            if role_members[member] != str(bot.user.id):
+                                remind_members.append(role_members[member])
+                    print(f"Reminding the following members: {remind_members}")
 
                     # Convert react_dict into a user that converts the reactions into green squares and non-reactions into red-squares
                     conv = {}
@@ -71,6 +123,7 @@ def run():
                                     conv[user].append("🟥")
                                 else:
                                     conv[user].append("🟩")
+
                     # initialize table
                     table = []
                     headers = settings.headers
@@ -89,15 +142,27 @@ def run():
                     embed = discord.Embed(title="Schedule")
                     embed.add_field(
                         name=headers[0],
-                        value="\n\n".join(list(df[headers[0]])),
+                        value="\n".join(list(df[headers[0]])),
                         inline=True,
                     )
+
                     embed.add_field(
-                        name="\u1CBC".join(server_emojis),
-                        value="\n\n".join("\u1CBC".join(row[1:]) for row in table[1:]),
+                        name=" | ".join(server_emojis),
+                        value="\n".join(" | ".join(row[1:]) for row in table[1:]),
                         inline=True,
                     )
-                    await ctx.send(embed=embed)
+
+                    if remind_members != []:
+                        for i in range(len(remind_members)):
+                            remind_members[i] = f"<@{remind_members[i]}>"
+                        reminder = f"Reminding: {' '.join(remind_members)}"
+                    else:
+                        reminder = ""
+
+                    if reminder == "":
+                        await ctx.send(embed=embed)
+                    else:
+                        await ctx.send(reminder, embed=embed)
                 else:
                     pass  # Nothing should happen here
             else:
@@ -108,14 +173,16 @@ def run():
                 break
 
     "Helper function to collect reactions from a prior schedule message."
-
     async def collate_table(msg):
         # There is an edge case that may cause a bug where if the bot's reactions are removed, it will not be able to run due to dimensionality failure
+        user_ids = []
         react_dict = {}
         for reaction in msg.reactions:
             reaction_str = str(reaction)
             if reaction_str in settings.emojis:
                 async for user in reaction.users():
+                    if user.id not in user_ids:
+                        user_ids.append(user.id)
                     if user == bot.user:
                         if reaction_str not in react_dict:
                             react_dict[reaction_str] = []
@@ -131,6 +198,13 @@ def run():
         print(total_users, react_dict)
         return (total_users, react_dict)
         # react_dict should have the structure of {emoji_id1:[list of usernames], emoji_id2:[list of usernames]}
+
+    "Helper function to get a list of role objects in a guild"
+    async def get_roles(ctx):
+        # Has to be used before the context is deleted
+        roles = list(ctx.guild.roles)
+        # roles = {role.id:role.name for role in roles}
+        return roles
 
     bot.run(settings.DISCORD_API_SECRET)
 
