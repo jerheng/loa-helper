ece0fa4bd89d8f2757dbdc3d9bd34bc81f158b69
jerheng <57416682+jerheng@users.noreply.github.com>
2024-01-09 20:02:04 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -74,7 +74,7 @@ def run():
         flag = False  # Flag to keep track of whether the message has been found.
 
         # Set oldest_first = False so that we only keep track of the latest schedule collated
-        async for message in ctx.channel.history(oldest_first=True):
+        async for message in ctx.channel.history(oldest_first=False):
             # print(message)
             # Delete command initialization message
             if message.author.bot:
